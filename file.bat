599180f00f8f2b3a9cddc0fe69f1db6d1e8858c0
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-10-21 15:29:45 -0700

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,5 +1,5 @@
 @echo off
-magick convert -background black -fill white -font Candice \ -size x400  label:"Sys3 is an elite hacker!" caption.jpg
+magick convert -background black -fill white -font Comic-Sans-MS -size x400  label:"ayylmao" caption.jpg
 magick mogrify -resize 400x *.jpg
 magick convert -append 1.jpg caption.jpg photo.jpg
 del 1.jpg
